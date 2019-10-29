51a16aff10e9a03b922c8e318a9435ccfa558ffb
marcelox3010 <marcelox3010@gmail.com>
2019-10-28 22:53:37 -0300

    create ngram files gram2, gram3 and gram4 - Rda

--- createNgramFiles.R
+++ createNgramFiles.R
@@ -8,25 +8,26 @@ suppressPackageStartupMessages(library("RWeka"))
 suppressPackageStartupMessages(library("qdap"))
 suppressPackageStartupMessages(library("ggplot2"))
 suppressPackageStartupMessages(library("ngram"))
+suppressPackageStartupMessages(library("rJava"))
 
 #sources
-suppressPackageStartupMessages(source("functions.R"))
+source("functions.R")
 
 #set.seed
 set.seed(3010)
 
 #reading and sampling
-perc <- 0.1
+perc <- 0.01
 
-blogs_en        <- readLines("final/en_US/en_US.blogs.txt", encoding = "UTF-8")
+blogs_en        <- readLines("final/en_US/en_US.blogs.txt", encoding = "UTF-8", skipNul = TRUE)
 t_blogs_en      <- sample(blogs_en, (length(blogs_en)*perc))
 remove(blogs_en)
 
-news_en       <- readLines("final/en_US/en_US.news.txt", encoding = "UTF-8")
+news_en       <- readLines("final/en_US/en_US.news.txt", encoding = "UTF-8", skipNul = TRUE)
 t_news_en     <- sample(news_en, (length(news_en)*perc))
 remove(news_en)
 
-twitter_en    <- readLines("final/en_US/en_US.twitter.txt", encoding = "UTF-8")
+twitter_en    <- readLines("final/en_US/en_US.twitter.txt", encoding = "UTF-8", skipNul = TRUE)
 t_twitter_en  <- sample(twitter_en, (length(twitter_en)*perc))
 remove(twitter_en)
 
@@ -41,12 +42,21 @@ t_all         <- removeNumbers(t_all)
 t_all         <- tolower(t_all)
 t_all         <- t_all[which(t_all!="")]
 
-#create NGRAM - 2, 3, 4
 
-gram2   <- my_ngram(t_all, 2)
-gram3   <- my_ngram(t_all, 3)
-gram4   <- my_ngram(t_all, 4)
+write(t_all, file = "sample.out", ncolumns = 1)
+remove(t_all)
 
+#create ngram
+options(java.parameters = "-Xmx4000m")
+
+linhas        <- readLines("sample.out", encoding = "UTF-8", skipNul = TRUE)
+
+gram2 <- my_ngram(linhas, 2)
+save(gram2, file="gram2.Rda")
+gram3 <- my_ngram(linhas, 3)
+save(gram3, file="gram3.Rda")
+gram4 <- my_ngram(linhas, 4)
+save(gram4, file="gram4.Rda")
 
 
 
