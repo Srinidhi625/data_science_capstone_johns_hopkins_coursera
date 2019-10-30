1bd3b57789ef08bc57f14958fc71ecc4d1bc9982
Marcelo Gadelha <marcelox3010@gmail.com>
2019-10-30 15:35:30 -0300

    2%

--- App.R
+++ App.R
@@ -59,9 +59,9 @@ server <- function(input, output) {
   
   source("functions.R")
   
-  gram2 <<- readRDS(file = "gram2_0.03.Rda")
-  gram3 <<- readRDS(file = "gram3_0.03.Rda")
-  gram4 <<- readRDS(file = "gram4_0.03.Rda")
+  gram2 <<- readRDS(file = "gram2_0.02.Rda")
+  gram3 <<- readRDS(file = "gram3_0.02.Rda")
+  gram4 <<- readRDS(file = "gram4_0.02.Rda")
   
   output$predictWord <- renderText({
     
